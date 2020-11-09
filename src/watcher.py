4f281fc6075b2d3818b8c498a82923d740a895fe
Sven <glaglasven@live.com>
2020-11-09 14:52:30 +0100

    Motion Test

---
+++ src/watcher.py
@@ -0,0 +1,20 @@
+from gpiozero import MotionSensor
+
+pir = MotionSensor(4)
+
+
+def trigger():
+    print('hi')
+
+
+def start_watch():
+    """
+    Infinite loop that waits for the PIR to trigger.
+    Once triggered, call a sound.
+    """
+    while True:
+        pir.when_motion = trigger()
+
+
+if __name__ == "__main__":
+    start_watch()
